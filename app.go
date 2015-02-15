e54289ebb7744eb1992e5c474b6d1e4ac9fa517c
Michael Raimondi <mike@mikeraimondi.com>
2015-02-14 19:25:01 -0500

    Implement SSL

--- app.go
+++ app.go
@@ -3,15 +3,24 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
 )
 
 var googleKey string
+var securePort string
+var insecurePort string
 
 func init() {
 	googleKey = os.Getenv("GKEY")
+	portBase, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		log.Fatal("Invalid port number", err)
+	}
+	securePort = strconv.Itoa(portBase + 443)
+	insecurePort = strconv.Itoa(portBase + 80)
 }
 
 func main() {
@@ -19,8 +28,12 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir("frontend")))
 
 	http.HandleFunc("/query", root)
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-	if err != nil {
+	go func() {
+		if err := http.ListenAndServeTLS(":"+securePort, "cert.pem", "key.pem", nil); err != nil {
+			log.Fatal("ListenAndServeTLS: ", err)
+		}
+	}()
+	if err := http.ListenAndServe(":"+insecurePort, http.HandlerFunc(secureRedirect)); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
@@ -48,4 +61,9 @@ func root(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	json.NewEncoder(w).Encode(nearestStations)
 	return
+}
+
+func secureRedirect(w http.ResponseWriter, r *http.Request) {
+	host, _, _ := net.SplitHostPort(r.Host)
+	http.Redirect(w, r, "https://"+host+":"+securePort+r.RequestURI, http.StatusMovedPermanently)
 }
