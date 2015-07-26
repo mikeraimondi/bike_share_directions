918c89729bd7cd689e66c05e125c57249b3f6883
Michael Raimondi <mike@mikeraimondi.com>
2015-07-26 16:51:47 -0400

    'go get' before build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd app/backend
+go get
 CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -installsuffix netgo -o bike_share_directions .
 cd ../..
 node_modules/.bin/bower install --config.interactive=false
